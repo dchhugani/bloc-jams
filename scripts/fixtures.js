58ff4260e36df3191bed7ead80fd620c690d9a10
Deepak Chhugani <dchhugani@gmail.com>
2017-03-29 19:42:37 -0400

    checkpoint 32 commit ex. for parse Int

---
+++ scripts/fixtures.js
@@ -0,0 +1,47 @@
+// Example Album
+ var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21'},
+         { title: 'Magenta', duration: '2:15'}
+     ]
+ };
+ 
+ // Another Example Album
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+
+// Assignment 25 Example Album
+ var albumToti = {
+     title: 'Vales Paloma',
+     artist: 'Jordi Chota',
+     label: 'CP',
+     year: '6969',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Habla, Biyete', duration: '2:01' },
+         { title: 'Chiri, chiri, chiri', duration: '5:15' },
+         { title: 'Te la meto duro', duration: '3:27'},
+         { title: 'Encebollado quien quiere?', duration: '3:17' },
+         { title: 'Tu ñaña es rica', duration: '2:50'}
+     ]
+ };
